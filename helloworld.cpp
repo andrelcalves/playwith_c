180eacdfe14adc73c8d026bb797c9a3b33c0c648
André Luis Carvalho Alves <andrealves@Andres-MacBook-Pro.local>
2023-12-16 22:43:30 -0300

    playaroud with datatypes

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+// This code is for draft and test only
 /*
 data types
 
@@ -7,17 +7,21 @@ chat    (1 byte)  | %c
 int     (4 bytes) | %d
 float   (4 bytes) | %f
 double  (8 bytes) | %lf
+
+to print bytes use %zu
 */
 
 
-void fprinttest(const char *hello);
+void funcHelloWorld(const char *hello);
+void funcUserInputData(void);
 
 int main(){
-    fprinttest("Hello, world!\n");
+    funcHelloWorld("Hello, world!\n");
+
     return 0;
 }
 
-void fprinttest(const char *hello){
+void funcHelloWorld(const char *hello){
     int x;
     
     while(*hello){
@@ -29,6 +33,37 @@ void fprinttest(const char *hello){
       printf("%d", x);      
     }
 
+    double y;
+    y = 5.5e6;
+
+    printf("\n %lf",y);
+
+    //the char type in fact is stored as integer so if you print with %d u can see the value of the caracter as showing below
+    char letterA = 'a';
+    printf("\nletter a = %d",letterA);
+
+    //get the size of the variables in bytes
+    int age = 44;
+    double number = 5.5;
+    printf("\nint size in bytes = %zu",sizeof(age));
+    printf("\nnumber size in bytes = %zu \n",sizeof(number));
+
+    funcUserInputData();
+}
+
+void funcUserInputData(void){
+    // get date from user interaction
+    int age;
+    printf("Enter age value: ");
+    scanf("%d",&age);
+    printf("Age =%d",age);
+
+    // get more than one value
+    char gender;
+    printf("Enter your age than you gender: ");
+    scanf("%d %c",&age, &gender);
+
+    printf("\nYou have %d years old and is a %c", age, gender);
 }
 
 
